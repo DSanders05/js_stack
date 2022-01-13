9e792e0bf40e6373de44b4d0a74ab6fd92b3d6af
Drake Sanders <drake_sanders12@icloud.com>
2022-01-13 13:34:45 -0600

    First JSX assignment update

---
+++ js_fundamentals/morning_algos/w1d4.js
@@ -0,0 +1,19 @@
+//merge sort algo
+let x = [1,5,6,6,8,14]
+
+let y = [-3, 6, 9, 10, 15,20,22,28]
+
+
+
+//create a function that accepts 2 already sorted arrays and combines them into one sorted array and returns the sorted array
+function combine(arr1, arr2){
+    let result = []
+    
+    //hint: create two variables to keep track of index numbers of each respective arrays
+
+    //compare the values at the beginning of each input arr
+
+
+}
+
+console.log(combine(x,y)) //[-3,1,5,6,6,6,8,9,10,14,15,20,22,28]
---
+++ react_assignments/first_react_app/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>*Learn React</li>
+        <li>*Get a job</li>
+        <li>*Program Lots</li>
+        <li>*Win</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
