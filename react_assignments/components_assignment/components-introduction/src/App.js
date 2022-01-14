79798eb3adc10a028c82e420056b63452c58ddcc
Drake Sanders <drake_sanders12@icloud.com>
2022-01-13 21:50:15 -0600

    Putting It Together Turn In

--- react_assignments/components_assignment/components-introduction/src/App.js
+++ react_assignments/components_assignment/components-introduction/src/App.js
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       {/* information you pass down to a componenet is called props or properties */}
-      <PersonCard firstName= {"Drak"} lastName= {"Person"} age={28} hairColor= {"Brown"}></PersonCard>
+      <PersonCard firstName= {"Drak"} lastName= {"Person"} age={26} hairColor= {"Brown"}></PersonCard>
       <hr />
-      <PersonCard firstName= {"Mac"} lastName={"Donald"} age={32} hairColor= {"Black"}></PersonCard>
+      <PersonCard firstName= {"Mac"} lastName={"Donald"} age={18} hairColor= {"Black"}></PersonCard>
       <hr />
-      <PersonCard firstName= {"Paige"} lastName= {"Books"} age={14} hairColor= {"Red"}></PersonCard>
+      <PersonCard firstName= {"Paige"} lastName= {"Books"} age={42} hairColor= {"Red"}></PersonCard>
 
     </div>
   );
--- react_assignments/components_assignment/components-introduction/src/Components/PersonCard.js
+++ react_assignments/components_assignment/components-introduction/src/Components/PersonCard.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'; //importing React and Components so that we can create React component
 
 class PersonCard extends Component {
-    render() { //can only return one parent. Structure others inside one main div to return mult elements
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            age:this.props.age
+        };
+    }
+
+    render() {
+        //can only return one parent. Structure others inside one main div to return mult elements
+
+        let birthdayClick = ()=>{
+            this.setState({age:this.state.age+1});
+        }
+
         const { firstName, lastName, age, hairColor} = this.props
+
         return <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {this.state.age}</p>
             <p>Hair Color: {hairColor}</p>
-            
+            <button onClick= {birthdayClick}>Birthday button for {firstName} {lastName}</button>
         </div>
     }
 }
